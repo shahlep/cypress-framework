1146971d34122df50db5a37f6b5db98b1f06e213
Shah rahman <toshah.rahman@gmail.com>
2022-03-08 17:40:06 +0200

    login steps and cypress json upadte

--- cypress/integration/cucumber-tests/login/loginSteps.js
+++ cypress/integration/cucumber-tests/login/loginSteps.js
@@ -1,3 +1,16 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 
 
+Given('I visit login page',()=>{
+    cy.visit('https://www.saucedemo.com/')
+})
+
+When('I submit login credentials',()=>{
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+})
+
+Then('I should see homepage',()=>{
+    cy.url().should('contain','inventory.html')
+})
